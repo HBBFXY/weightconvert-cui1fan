3284cfb9b3ce052f76f4acd0080fb5754848da70
cui1fan <1819285039@qq.com>
2025-09-09 19:23:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-weight_input = input()
-num = float(weight_input[:-2])
-unit = weight_input[-2:]
-if unit =="kg":
-  pound = num * 2.2046
-  print(f"对应的英制重量单位为{pound:.3f}磅")
-elif unit =="pd":
-  kg = num / 2.2046
-  print(f"对应的公制重量为{kg:.3f}公斤")# 在这个文件下编写代码，题目具体要求见README.md文件
+def weight_convert():
+  s = input().strip()
+if s.endswith("kg):
+  kg = float(s[:-2])
+  pd = kg*2.2046
+  print("对应的英制重量为{：.3f}磅".format(pd))
+elif s.endswith("pd"):
+  pd_val = float(s[:-2])
+  kg_val = pd_val /2.2046
+  print("对应的公制重量为{:.3f}公斤".format(kg_val))
+else:
+  printf("输入格式错误，请以kg或pd结尾")这个文件下编写代码，题目具体要求见README.md文件
