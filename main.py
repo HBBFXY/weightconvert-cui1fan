d9519ccb1d0f7e2ad63dcb1f4a263b9652db417b
cui1fan <1819285039@qq.com>
2025-09-09 20:25:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def weight_converter():
   if input_str.endswith('kg'):
       kg = float(input_str[:-2])
       pd = kg * 2.2046
-      print(f"对应的英制重量为{pd.3f}磅")
+      print(f"对应的英制重量为{pd:.3f}磅")
   elif input_str.endswith('pd'):
       pd = float(input_str[:-2])
       kg = pd /2.2046 - 0.001
