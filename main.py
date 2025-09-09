9f2c77ab67b3bea4c199b2f6ad55bde5cd8c157b
cui1fan <1819285039@qq.com>
2025-09-09 20:00:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if input_str.endswith("kg"):
 elif input_str.endswith("pd"):
   pd = float(input_str[:-2])
   kg = pd /2.2046 - 0.001
-  print(f"对应的公制重量为{kg:.3f}公斤")
+  print(f"对应的公制重量为{kg: .3f}公斤")
 else:
   printf("输入格式错误，请以kg或pd结尾")
   if_name_=="_main_":
